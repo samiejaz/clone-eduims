9b96fdd66209ff53f4988726af0ae32b8609b5f3
Development <Development@DESKTOP-E4A0TS0>
2024-04-29 10:54:20 +0500

    added login background

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -71,124 +71,100 @@ const SignUp = () => {
   }
 
   return (
-    <Container
-      fluid
-      className="p-4 background-radial-gradient overflow-hidden d-flex justify-content-center align-items-center "
-    >
-      <Row>
-        <Col
-          md={6}
-          className="text-center text-md-start d-flex flex-column justify-content-center"
-        >
-          <h1
-            className="my-5 display-3 fw-bold ls-tight px-3"
-            style={{ color: "#c150c1" }}
-          >
-            The best offer
-            <br />
-            <span style={{ color: "#85b8c2" }}>for your business</span>
-          </h1>
-
-          <p className="px-3" style={{ color: "#a2a2a2" }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-            itaque accusantium odio, soluta, corrupti aliquam quibusdam tempora
-            at cupiditate quis eum maiores libero veritatis? Dicta facilis sint
-            aliquid ipsum atque?
-          </p>
-        </Col>
-
-        <Col md={6} className="position-relative">
-          <div
-            id="radius-shape-1"
-            className="position-absolute rounded-circle shadow-5"
-          ></div>
-          <div id="radius-shape-2" className="position-absolute shadow-5"></div>
-
-          <Card className="my-5 bg-glass">
-            <Card.Body className="p-5">
-              <div className="text-center mb-5">
-                <i
-                  className="fas fa-crow fa-3x me-3"
-                  style={{ color: "#709085" }}
-                />
-              </div>
+    <>
+      <div
+        id="login-container"
+        className="flex justify-content-end min-h-screen mx-5"
+      >
+        <Card className="my-5 bg-glass w-50">
+          <Card.Body className="p-5">
+            <div className="text-center mb-5">
+              <i
+                className="fas fa-crow fa-3x me-3"
+                style={{ color: "#709085" }}
+              />
+            </div>
+            <Row>
+              <h1 className="text-center mb-5">Login</h1>
+            </Row>
+            <form onSubmit={handleSubmit(onSubmit)} method="post" noValidate>
               <Row>
-                <h1 className="text-center mb-5">Login</h1>
+                <Form.Group className="mb-2">
+                  <Form.Label className="fs-5">Username/Email</Form.Label>
+                  <input
+                    type="text"
+                    id="LoginName"
+                    name="LoginName"
+                    className="form-control p-3"
+                    placeholder="Enter your username/email..."
+                    {...register("LoginName", {
+                      required: "Username is required!",
+                    })}
+                  />
+                </Form.Group>
+                <p className="text-danger">{errors.LoginName?.message}</p>
               </Row>
-              <form onSubmit={handleSubmit(onSubmit)} method="post" noValidate>
-                <Row>
-                  <Form.Group className="mb-2">
-                    <Form.Label className="fs-5">Username/Email</Form.Label>
-                    <input
-                      type="text"
-                      id="LoginName"
-                      name="LoginName"
-                      className="form-control p-3"
-                      placeholder="Enter your username/email..."
-                      {...register("LoginName", {
-                        required: "Username is required!",
-                      })}
-                    />
-                  </Form.Group>
-                  <p className="text-danger">{errors.LoginName?.message}</p>
-                </Row>
-                <Row>
-                  <Form.Group className="mb-4">
-                    <Form.Label className="fs-5">Password</Form.Label>
-                    <input
-                      type="password"
-                      id="Password"
-                      name="Password"
-                      className="form-control p-3"
-                      placeholder="Enter your password..."
-                      {...register("Password", {
-                        required: "Password is required!",
-                      })}
-                    />
-                  </Form.Group>
-                  <p className="text-danger">{errors.Password?.message}</p>
-                </Row>
+              <Row>
+                <Form.Group className="mb-4">
+                  <Form.Label className="fs-5">Password</Form.Label>
+                  <input
+                    type="password"
+                    id="Password"
+                    name="Password"
+                    className="form-control p-3"
+                    placeholder="Enter your password..."
+                    {...register("Password", {
+                      required: "Password is required!",
+                    })}
+                  />
+                </Form.Group>
+                <p className="text-danger">{errors.Password?.message}</p>
+              </Row>
 
-                <Button
-                  className="w-100 mb-4 fs-5 p-2"
-                  size="md"
-                  type="submit"
-                  disabled={mutation.isPending}
-                >
-                  {mutation.isPending ? (
-                    <>
-                      <Spinner
-                        as="span"
-                        animation="border"
-                        size="sm"
-                        role="status"
-                        aria-hidden="true"
-                      />
-                      <span> Logging In...</span>
-                    </>
-                  ) : (
-                    "Login"
-                  )}
-                </Button>
-                <div className="text-center mt-2">
-                  <p>
-                    Developed at{" "}
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="http://www.edusoftsolutions.com"
-                      className="text-center"
-                    >
-                      Edusoft System Solutions
-                    </a>
-                  </p>
-                </div>
-              </form>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+              <Button
+                className="w-100 mb-4 fs-5 p-2"
+                size="md"
+                type="submit"
+                style={{
+                  background: "#7ED957",
+                  border: "none",
+                  outline: "none",
+                }}
+                disabled={mutation.isPending}
+              >
+                {mutation.isPending ? (
+                  <>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    <span> Logging In...</span>
+                  </>
+                ) : (
+                  "Login"
+                )}
+              </Button>
+              <div className="text-center mt-2">
+                <p>
+                  Developed at{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="http://www.edusoftsolutions.com"
+                    className="text-center"
+                  >
+                    Edusoft System Solutions
+                  </a>
+                </p>
+              </div>
+            </form>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
   )
 }
 
